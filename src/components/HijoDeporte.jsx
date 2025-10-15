845098e88655bca894bf0a7fbae9f0ca6e6c4533
angeldzz <pintodiazangel@gmail.com>
2025-10-15 09:23:56 +0200

    Mensaje en padre enviado con datos de hijo

--- src/components/HijoDeporte.jsx
+++ src/components/HijoDeporte.jsx
@@ -1,20 +1,15 @@
 import React, { Component } from 'react'
 
 export default class HijoDeporte extends Component {
-    state = {
-        favorito : ""
-    }
-    mostarFavorito = () => {
-        this.setState({
-            favorito:this.props.nombre
-        })
-    }
+  seleccionarFavorito = () => {
+    let deporte = this.props.nombre;
+    this.props.mostrarFavorito(deporte);
+  }
   render() {
     return (
       <div>
         <h3 style={{color:"green"}}>Deporte: {this.props.nombre}</h3>
-        <h2>Su deporte favorito es : {this.state.favorito}</h2>
-        <button onClick={this.mostarFavorito} type="button">Seleccionar Favorito</button>
+        <button onClick={this.seleccionarFavorito} type="button">Seleccionar Favorito</button>
       </div>
     )
   }
--- src/components/PadreDeportes.jsx
+++ src/components/PadreDeportes.jsx
@@ -3,14 +3,23 @@ import HijoDeporte from './HijoDeporte'
 
 export default class PadreDeportes extends Component {
     deportes = ["Football","Tenis","Padel","Petanca"];
+    state = {
+        favorito : ""
+    }
+    mostrarFavorito = (deporteSeleccionado) => {
+        this.setState({
+            favorito:deporteSeleccionado
+        })
+    }
   render() {
     return (
         <div>
             <h1 style={{color:"red"}}>Padre Deportes</h1>
             <hr />
+            <h2>Su deporte favorito es : {this.state.favorito}</h2>
             {
             this.deportes.map((deporte, index) => {
-                return(<HijoDeporte key={index} nombre={ deporte }/>)
+                return(<HijoDeporte mostrarFavorito={this.mostrarFavorito} key={index} nombre={ deporte }/>)
             })
             }
         </div>
